f28335de5fa8541bf433f7273c928e8ef880e1e0
quangvinht <96629186+quangvinht@users.noreply.github.com>
2022-09-07 17:48:51 +0700

    chot don

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpRequest = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
+    baseURL: 'https://fakestoreapi.com/',
 });
 
 export const get = async (path) => {
